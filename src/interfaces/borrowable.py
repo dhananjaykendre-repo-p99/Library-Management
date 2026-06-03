0064587efe8e8d3470ea3d74949e62e7b96ce2ee
dhananjaykendre-repo-p99 <dhananjay.kendre@p99soft.com>
2026-06-03 10:36:01 +0530

    add Borrowable and Searchable interfaces

---
+++ src/interfaces/borrowable.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+class Borrowable(ABC):
+    @abstractmethod
+    def borrow_book(self):
+        pass
+
+    @abstractmethod
+    def return_book(self):
+        pass
+
+    @abstractmethod
+    def is_available(self):
+        pass
